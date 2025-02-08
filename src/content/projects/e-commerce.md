20315a25ed643d1c8b31eb0c843a501d8d3b4ff2
alejandrotsx <alejandrosunuc@gmail.com>
2025-02-07 19:43:49 -0600

    Add Tailwind CSS integration and project structure with internationalization support

---
+++ src/content/projects/e-commerce.md
@@ -0,0 +1,11 @@
+---
+title: E-commerce
+description: An ecommerce
+img: e-commerce.png
+tags: ["react", "tailwind"]
+link: https://ola.netlify.ul
+github: https://github.com/alejandroimp/desafio
+lang: es
+---
+
+ola
---
+++ src/i18n/ui.ts
@@ -0,0 +1,39 @@
+export const languages = {
+  es: 'Español',
+  en: 'English'
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+  en: {
+    'nav.title': 'Web Developer',
+    'header.presentation': `Hi, I'm `,
+    'section.aboutme.title': 'About me',
+    'section.aboutme.description': `Initiative-taking Frontend Developer with solid knowledge in frameworks like React for Frontend and ExpressJS, NestJs for Back-End work. Looking for an opportunity to apply my technical skills and passion for web development in a professional environment. With a strong self-learning ability and experience gained through personal and academic projects, I look forward to contributing to a team's success and professional growth in the web development field`,
+    'section.projects.title': 'Projects',
+    'section.technologies.title': 'Technologies',
+    'section.contact.title': 'Contact me',
+    'section.contact.form.title': 'Contact',
+    'section.contact.form.name': 'Name',
+    'section.contact.form.email': 'Email',
+    'section.contact.form.message': 'Message',
+    'section.contact.form.submit': 'Submit'
+  },
+  es: {
+    'nav.title': 'Desarrollador Web',
+    'header.presentation': 'Hola, soy ',
+    'section.aboutme.title': 'Acerca de mi',
+    'section.aboutme.description': `Desarrollador autodidacta con sólidos conocimientos en framework de Front-End, manejo de API REST, experiencia la UI y UX, con conocimiento de la construcción del Back-End. Buscando una oportunidad para aplicar mis habilidades técnicas y mi pasión por el desarrollo web en un entorno profesional. Con una gran capacidad de autoaprendizaje y experiencia adquirida a través de proyectos personales y académicos, espero contribuir al éxito y crecimiento profesional de un equipo en el campo del desarrollo web. `,
+    'section.projects.title': 'Proyectos',
+    'section.technologies.title': 'Tecnologias',
+    'section.contact.title': 'Contactame',
+    'section.contact.form.title': 'Contacto',
+    'section.contact.form.name': 'Nombre',
+    'section.contact.form.email': 'Correo',
+    'section.contact.form.message': 'Mensaje',
+    'section.contact.form.submit': 'Enviar'
+  }
+} as const;
+
+export const showDefaultLang = false;
